8cc80cc81aade67e1b3d1ef274726dd9c646a0b8
Jan Krassnigg <Jan@Krassnigg.de>
2020-01-11 00:23:59 +0100

    Player Health

--- Prefabs/Player2_data/Player2.ts
+++ Prefabs/Player2_data/Player2.ts
@@ -44,38 +44,47 @@ export class Player2 extends ez.TickedTypescriptComponent {
 
     Tick(): void {
 
-        // character controller update
-        {
-            let msg = new ez.MsgMoveCharacterController();
+        if (this.health > 0) {
 
-            msg.Jump = this.input.GetCurrentInputState("Jump", false) > 0.5;
-            msg.MoveForwards = this.input.GetCurrentInputState("MoveForwards", false);
-            msg.MoveBackwards = this.input.GetCurrentInputState("MoveBackwards", false);
-            msg.StrafeLeft = this.input.GetCurrentInputState("StrafeLeft", false);
-            msg.StrafeRight = this.input.GetCurrentInputState("StrafeRight", false);
-            msg.RotateLeft = this.input.GetCurrentInputState("RotateLeft", false);
-            msg.RotateRight = this.input.GetCurrentInputState("RotateRight", false);
-            msg.Run = this.input.GetCurrentInputState("Run", false) > 0.5;
-            msg.Crouch = this.input.GetCurrentInputState("Crouch", false) > 0.5;
+            // character controller update
+            {
+                let msg = new ez.MsgMoveCharacterController();
 
-            this.characterControler.SendMessage(msg);
+                msg.Jump = this.input.GetCurrentInputState("Jump", false) > 0.5;
+                msg.MoveForwards = this.input.GetCurrentInputState("MoveForwards", false);
+                msg.MoveBackwards = this.input.GetCurrentInputState("MoveBackwards", false);
+                msg.StrafeLeft = this.input.GetCurrentInputState("StrafeLeft", false);
+                msg.StrafeRight = this.input.GetCurrentInputState("StrafeRight", false);
+                msg.RotateLeft = this.input.GetCurrentInputState("RotateLeft", false);
+                msg.RotateRight = this.input.GetCurrentInputState("RotateRight", false);
+                msg.Run = this.input.GetCurrentInputState("Run", false) > 0.5;
+                msg.Crouch = this.input.GetCurrentInputState("Crouch", false) > 0.5;
+
+                this.characterControler.SendMessage(msg);
+            }
+
+            // look up / down
+            {
+                let up = this.input.GetCurrentInputState("LookUp", false);
+                let down = this.input.GetCurrentInputState("LookDown", false);
+
+                this.headBone.ChangeVerticalRotation(down - up);
+            }
         }
 
-        // look up / down
-        {
-            let up = this.input.GetCurrentInputState("LookUp", false);
-            let down = this.input.GetCurrentInputState("LookDown", false);
-
-            this.headBone.ChangeVerticalRotation(down - up);
-        }
+        ez.Debug.Draw2DText("Health: " + Math.ceil(this.health), new ez.Vec2(10, 10), ez.Color.White(), 32);
     }
 
     static RegisterMessageHandlers() {
 
         ez.TypescriptComponent.RegisterMessageHandler(ez.MsgInputActionTriggered, "OnMsgInputActionTriggered");
+        ez.TypescriptComponent.RegisterMessageHandler(ez.MsgDamage, "OnMsgMsgDamage");
     }
 
     OnMsgInputActionTriggered(msg: ez.MsgInputActionTriggered): void {
+
+        if (this.health <= 0)
+            return;
 
         if (msg.TriggerState == ez.TriggerState.Activated) {
 
@@ -117,7 +126,45 @@ export class Player2 extends ez.TickedTypescriptComponent {
             }
 
         }
+    }
 
+    health: number = 100;
+
+    OnMsgMsgDamage(msg: ez.MsgDamage): void {
+
+        if (this.health <= 0)
+            return;
+
+        this.health -= msg.Damage * 2;
+
+        if (this.health <= 0) {
+            let owner = this.GetOwner();
+
+            let camera = owner.FindChildByName("Camera");
+
+            let camPos = camera.GetGlobalPosition();
+
+            let go = new ez.GameObjectDesc();
+            go.LocalPosition = camera.GetGlobalPosition();
+            go.Dynamic = true;
+
+            let rbCam = ez.World.CreateObject(go);
+
+            let rbCamActor = ez.World.CreateComponent(rbCam, ez.PxDynamicActorComponent);
+            let rbCamSphere = ez.World.CreateComponent(rbCam, ez.PxShapeSphereComponent);
+            rbCamSphere.Radius = 0.3;
+            rbCamSphere.CollisionLayer = 2; // debris
+            let rbCamLight = ez.World.CreateComponent(rbCam, ez.PointLightComponent);
+            rbCamLight.LightColor = ez.Color.DarkRed();
+            rbCamLight.Intensity = 200;
+
+            rbCamActor.Mass = 30;
+            rbCamActor.LinearDamping = 0.5;
+            rbCamActor.AngularDamping = 0.99;
+            rbCamActor.AddAngularForce(ez.Vec3.CreateRandomPointInSphere());
+
+            camera.SetParent(rbCam);
+        }
     }
 }
 
